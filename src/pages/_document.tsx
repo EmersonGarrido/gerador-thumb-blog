2119fc8b006ca980e4065c4643f137bef87b4b19
EmersonGarrido <emersongarrido@hotmail.com.br>
2022-06-30 17:27:20 -0400

    chore: send vercel

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -22,7 +22,6 @@ class MyDocument extends Document {
             href='https://fonts.googleapis.com/css2?family=Inter:wght@100;200;300;400;500;600;700;800;900&family=Poppins:wght@100;200;300;400;500;600;700;800;900&family=Lato&display=swap'
             rel='stylesheet'
           />
-          <script src="https://cdn.jsdelivr.net/npm/tw-elements/dist/js/index.min.js"></script>
           <title>Gerador de Stories.com</title>
         </Head>
         <body>
